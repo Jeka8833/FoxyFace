ad1b9091880bfbbf4ba055e487ad4ef28361d9e3
Jeka8833 <eadercach@gmail.com>
2025-08-29 11:22:53 +0300

    Fix WSL sound device

--- FoxyFace/src/ui/UiSoundUtil.py
+++ FoxyFace/src/ui/UiSoundUtil.py
@@ -1,7 +1,5 @@
 import logging
-
-import sounddevice
-import soundfile
+from pathlib import Path
 
 from AppConstants import AppConstants
 
@@ -9,24 +7,24 @@ __logger = logging.getLogger(__name__)
 
 
 def play_start_sound():
-    try:
-        data, fs = soundfile.read(AppConstants.get_application_root() / 'Assets' / 'start.opus')
-        sounddevice.play(data, fs)
-    except Exception:
-        __logger.warning("Failed to play sound", exc_info=True, stack_info=True)
+    __run_play_for_file(AppConstants.get_application_root() / 'Assets' / 'start.opus')
 
 
 def play_good_sound():
-    try:
-        data, fs = soundfile.read(AppConstants.get_application_root() / 'Assets' / 'good.opus')
-        sounddevice.play(data, fs)
-    except Exception:
-        __logger.warning("Failed to play sound", exc_info=True, stack_info=True)
+    __run_play_for_file(AppConstants.get_application_root() / 'Assets' / 'good.opus')
 
 
 def play_fail_sound():
+    __run_play_for_file(AppConstants.get_application_root() / 'Assets' / 'fail.opus')
+
+
+def __run_play_for_file(file: Path):
     try:
-        data, fs = soundfile.read(AppConstants.get_application_root() / 'Assets' / 'fail.opus')
+        # WSL or other platforms that doesn't support sounddevice
+        import sounddevice
+        import soundfile
+
+        data, fs = soundfile.read(file)
         sounddevice.play(data, fs)
     except Exception:
         __logger.warning("Failed to play sound", exc_info=True, stack_info=True)
